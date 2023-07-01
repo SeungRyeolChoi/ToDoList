c213e8ba491e08932f651f6ee02433b8fc266762
ChoiSeungRyeol <csy1736@likelion.org>
2023-07-01 22:41:10 +0900

    Todolist

---
+++ src/main/java/com/mysite/todo/ToDoList/ToDoEntity.java
@@ -0,0 +1,31 @@
+package com.mysite.todo.ToDoList;
+
+import javax.persistence.*;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+// @Controller 처럼 @Entity라고 작성해야한다.
+public class ToDoEntity {
+
+
+    @Id // 기본키로 설정한다.
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // 데이터를 저장할 때 해당 속성에 따로 값을
+    // 셋팅하지 않아도 1씩 자동으로 증가
+    // strategy는 고유 번호를 생성하는 옵션
+    // GenerationType.IDENTITY는 해당 컬럼만의
+    // 독립적인 시퀀스를 생성하여 번호를 증가시킬 때
+    // 사용한다.
+    private Integer id;
+
+    @Column(length=200)
+    // Column의 세부 설정을 위해 사용
+    private String content;
+
+    @Column(nullable = false)
+    private Boolean completed;
+}
---
+++ src/main/java/com/mysite/todo/ToDoList/ToDoService.java
@@ -0,0 +1,42 @@
+package com.mysite.todo.ToDoList;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class ToDoService {
+    private final ToDoRepository toDoRepository;
+
+    public List<ToDoEntity> getList(){
+        return this.toDoRepository.findAll();
+    }
+
+    public void create(String content){
+        ToDoEntity toDoEntity = new ToDoEntity();
+        toDoEntity.setContent(content);
+        toDoEntity.setCompleted(false);
+        this.toDoRepository.save(toDoEntity);
+    }
+
+    @Transactional
+    public void delete(Integer id){
+        ToDoEntity toDoEntity = toDoRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("해당 아이템이 없습니다. id=" + id));
+
+        this.toDoRepository.delete(toDoEntity);
+    }
+
+    @Transactional
+    public void update(Integer id, String content){
+        ToDoEntity toDoEntity = toDoRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("해당 아이템이 없습니다. id=" + id));
+
+        toDoEntity.setContent(content);
+        this.toDoRepository.save(toDoEntity);
+    }
+}
